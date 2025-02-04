1e0d7fa84e4d8cacff71b5f43e0a8ef1cb252227
Surasia <74399067+Surasia@users.noreply.github.com>
2025-02-04 11:59:28 +0100

    extension: add functionality to buttons

--- addon/__init__.py
+++ addon/__init__.py
@@ -2,8 +2,12 @@
 Ekur - Simple coating import for Reclaimer to Blender.
 """
 
+import logging
+import platform
 import bpy
 import subprocess
+import urllib.request
+import urllib.error
 
 from pathlib import Path
 from bpy.types import AddonPreferences, Operator, Context
@@ -21,14 +25,44 @@ bl_info = {
     "support": "COMMUNITY",
 }
 
+# Just copied from Reclaimer's RMF importer :3
+package_version = bl_info["version"]
+package_version_string = ".".join(str(i) for i in package_version)
 
-class DownloadStringsOperator(Operator):
-    bl_idname = "ekur.downloadstrings"
-    bl_label = "Download Strings"
+STRINGS_URL = "https://github.com/Surasia/ReclaimerFiles/raw/refs/heads/master/strings.txt"
+VISORS_URL = "https://github.com/Surasia/ekur/raw/refs/heads/master/all_visors.json"
+
+
+class DownloadFilesOperator(Operator):
+    bl_idname = "ekur.downloadfiles"
+    bl_label = "Download Required Files"
 
     def execute(self, context: Context | None) -> set[str]:
         data = context.preferences.addons["bl_ext.user_default.ekur"].preferences.data_folder  # pyright: ignore[reportAttributeAccessIssue]
-        _save_path = f"{data}/strings.txt"
+        save_path = f"{data}/strings.txt"
+        visors_path = f"{data}/all_visors.json"
+        try:
+            with (
+                urllib.request.urlopen(STRINGS_URL) as response,
+                open(save_path, "wb") as out_file,
+            ):
+                _ = out_file.write(response.read())
+        except urllib.error.HTTPError as e:
+            logging.error(f"Failed to download strings.txt: {e}")
+            return {"CANCELLED"}
+
+        try:
+            with (
+                urllib.request.urlopen(VISORS_URL) as response,
+                open(visors_path, "w") as out_file,
+            ):
+                _ = out_file.write(response.read().decode("utf-8"))
+        except urllib.error.HTTPError as e:
+            logging.error(f"Failed to download all_visors.json: {e.status}")
+            return {"CANCELLED"}
+
+        _ = Path(f"{data.data_folder}/all_visors.json")
+
         return {"FINISHED"}
 
 
@@ -38,8 +72,25 @@ class DumpFilesOperator(Operator):
 
     def execute(self, context: Context | None) -> set[str]:
         data: EkurPreferences = context.preferences.addons["bl_ext.user_default.ekur"].preferences
-        ekur_save_path = Path(f"{data.data_folder}/ekur.exe")
-        _ = Path(f"{data.data_folder}/all_visors.json")
+        ekur_save_path = Path(f"{data.data_folder}/ekur")
+        ekur_url = f"https://github.com/Surasia/ekur/releases/download/{package_version_string}/ekur-{package_version_string}"
+        if platform.system() == "Windows":
+            ekur_save_path = Path(f"{ekur_save_path}.exe")
+            ekur_url = f"{ekur_url}.exe"
+
+        print(ekur_url)
+
+        if not ekur_save_path.exists():
+            try:
+                with (
+                    urllib.request.urlopen(ekur_url) as response,
+                    open(ekur_save_path, "wb") as out_file,
+                ):
+                    _ = out_file.write(response.read())
+            except urllib.error.HTTPError as e:
+                logging.error(f"Failed to download ekur: {e.status}")
+                return {"CANCELLED"}
+
         save_path = f"{data.data_folder}/strings.txt"
         _ = subprocess.run(
             [
@@ -79,13 +130,8 @@ class EkurPreferences(AddonPreferences):
         box.prop(self, "data_folder")
         box.prop(self, "deploy_folder")
         box2 = layout.box()
-        _ = box2.operator("ekur.downloadstrings")
+        _ = box2.operator("ekur.downloadfiles")
         _ = box2.operator("ekur.dumpfiles")
-
-
-# Just copied from Reclaimer's RMF importer :3
-package_version = bl_info["version"]
-package_version_string = ".".join(str(i) for i in package_version)
 
 
 def register():
@@ -94,7 +140,7 @@ def register():
     register_class(EkurPreferences)
     register_class(ImportProperties)
     register_class(RandomizeCoatingOperator)
-    register_class(DownloadStringsOperator)
+    register_class(DownloadFilesOperator)
     register_class(DumpFilesOperator)
     bpy.types.Scene.import_properties = bpy.props.PointerProperty(type=ImportProperties)  # pyright: ignore[reportAttributeAccessIssue]
 
@@ -105,6 +151,6 @@ def unregister():
     unregister_class(CoatingImportPanel)
     unregister_class(ImportProperties)
     unregister_class(RandomizeCoatingOperator)
-    unregister_class(DownloadStringsOperator)
+    unregister_class(DownloadFilesOperator)
     unregister_class(DumpFilesOperator)
     del bpy.types.Scene.import_properties  # pyright: ignore[reportAttributeAccessIssue]
