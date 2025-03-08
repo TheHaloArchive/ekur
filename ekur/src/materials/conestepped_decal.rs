bf1fdcaeb1237a6fd846c7991abad29aac9f7799
Surasia <74399067+Surasia@users.noreply.github.com>
2025-03-08 22:44:18 +0100

    fix: silly clippy thing

--- ekur/src/materials/conestepped_decal.rs
+++ ekur/src/materials/conestepped_decal.rs
@@ -14,12 +14,12 @@ pub(super) fn handle_conestepped_decal(
     material: &mut Material,
 ) -> Result<()> {
     let mut conestepped_decal = ConesteppedDecal::default();
-    conestepped_decal.parallax_depth = f32_from_const(material, 0)?;
-    conestepped_decal.parallax_height_offset = f32_from_const(material, 4)?;
     get_post_texture(post_process, material, 20, TextureType::MacroConemap)?;
     get_post_texture(post_process, material, 48, TextureType::Control)?;
     get_post_texture(post_process, material, 80, TextureType::Normal)?;
     conestepped_decal.normal_intensity = f32_from_const(material, 112)?;
+    conestepped_decal.parallax_depth = f32_from_const(material, 0)?;
+    conestepped_decal.parallax_height_offset = f32_from_const(material, 4)?;
     material.conestepped_decal = Some(conestepped_decal);
     material.shader_type = ShaderType::ConesteppedDecal;
     Ok(())
